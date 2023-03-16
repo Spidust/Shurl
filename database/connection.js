16d8b20c7455b85003c37df9ce73c917d84bc2bd
Spidust <tranhaidangg2@gmail.com>
2023-03-16 18:58:43 +0700

    remove connect string log

--- database/connection.js
+++ database/connection.js
@@ -1,16 +1,5 @@
-// const mysql = require("mysql2");
-
-// const connection = mysql.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     database: 'test'
-// })
-
-// module.exports = connection;
-
 const { Client } = require('pg');
 const connectionString = process.env.CONNECT_STRING;
-console.log(connectionString);
 const client = new Client({
     connectionString
 });
